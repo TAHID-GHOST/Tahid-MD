233ab78cf1e5a82fe715834b1a3ce106cc491025
TAHID-GHOST <mdtahid41210@gmail.com>
2026-01-20 12:13:31 +0600

    Update bot settings for name and owner

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Tahid Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Tahid Bot",
+  botOwner: 'Minato', // Your name
+  ownerNumber: '8801719741293', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
